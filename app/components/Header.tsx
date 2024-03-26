955cca76b128114108f2cff657b298dcb9d927bb
Dan Reale <danreale10@gmail.com>
2024-03-25 22:49:21 -0400

    added header color

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,9 @@ export default function Header({}) {
     <>
       <div className="flex justify-center text-center py-5 items-center">
         <Link to="/">
-          <h1 className="text-4xl py-2">Equibase</h1>
+          <h1 className="text-4xl py-2 text-green-700">Equibase</h1>
         </Link>
-        <GiHorseshoe size={42} />
+        <GiHorseshoe size={42} color="grey" />
       </div>
     </>
   );
